add7263b51ebba5ecf3bc3f4ba3fbe664246c642
Ido Ben-Natan <ido.bennatan@gmail.com>
2014-07-08 17:59:14 +0300

    adding local storage service

--- app.js
+++ app.js
@@ -1,30 +1,56 @@
 (function(window, angular) {
 
-  angular.module('app', []).
+  angular.module('app', [])
+    .controller('todoCtrl', todoCtrl)
+    .factory('localTodo', localTodoService); 
 
-  controller('todoCtrl', todoCtrl); 
+  function todoCtrl(localTodo) {
+    //localTodo.add({ title: 'test', desc: 'a desc', complete: false  });
 
-  function todoCtrl() {
-    this.tasks = [];
+    this.tasks = localTodo.get();
     this.task = {};
 
-    this.addTask = function(task) {
-      if(task.complete == null) {
-        task.complete = false;
-        this.tasks.push(task);
-        this.task = {};
-      } else {
-        this.task = {};
+    this.saveTask = function(task) {
+      if(task && task.complete == null) {
+        this.tasks.push({
+          title: task.title,
+          desc: task.desc,
+          complete: false
+        });
       }
+      this.task = {};
+      console.log(this.tasks)
+      localTodo.set(this.tasks);
     };
     
     this.removeTask = function(task) {
       var index = this.tasks.indexOf(task);
       this.tasks.splice(index, 1);
+      localTodo.set(this.tasks);
     };
 
     this.editTask = function(task) {
       this.task = task;
+    };
+  };
+
+  function localTodoService() {
+    var tasks = null;
+    try {
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+    } catch(error) {
+      tasks = [{title: 'first task', desc: 'you should write more tasks', complete: false}];
+    }
+
+    localStorage.setItem('tasks', angular.toJson(tasks));
+
+    return {
+      get: function() {
+        return JSON.parse(localStorage.getItem('tasks'));
+      },
+      set: function(tasks) {
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+      }
     };
   };
 
